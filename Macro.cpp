461ad7420807788cad26757f71a562729192b2b1
Paras Bhandari <50918320+gamedev-parash@users.noreply.github.com>
2022-09-03 21:10:37 +0545

    Macro

---
+++ Macro.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+#define SIMPLIFY(x)(x)*(x)
+int main()
+{
+cout <<18/SIMPLIFY(3+2);
+cout <<endl;
+}
---
+++ Macro2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+#define MUL(x,y)(x)*(y)
+int main()
+{
+cout <<MUL(3,0);
+cout <<endl;
+}
