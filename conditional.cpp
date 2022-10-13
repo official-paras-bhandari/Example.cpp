3b7db04424b4e3cebc2cc0b3289dd6304998e95b
Niruta Dahal <nirutadahal502@gmail.com>
2022-10-13 18:32:32 +0545

    Refining code

--- conditional.cpp
+++ conditional.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-    int health = 13;
+    int health = 15;
     cout << ((health >0)?"i am still healthy.":"i am not healthy");
     return 0;
 }
--- recurisve.cpp
+++ recurisve.cpp
@@ -1,12 +1,23 @@
-#include<iostream>
-
+#include <iostream>
 using namespace std;
 
-void inqure()
-{
-    cout << "End";
-    inqure();
+int factorial(int);
+
+int main() {
+    int n, result;
+
+    cout << "Enter a non-negative number: ";
+    cin >> n;
+
+    result = factorial(n);
+    cout << "Factorial of " << n << " = " << result;
+    return 0;
 }
-int main(){
-    inqure();
+
+int factorial(int n) {
+    if (n > 1) {
+        return n * factorial(n - 1);
+    } else {
+        return 1;
+    }
 }
